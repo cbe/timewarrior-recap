45032461559e9d2bef519d9a4c82f863815b5096
cbe <cbe@users.noreply.github.com>
2023-01-24 13:18:44 +0100

    Application entrypoint parses stdin
    Prepares for working with the parsed JSON output of timewarrior

--- app/index.js
+++ app/index.js
@@ -1,4 +1,35 @@
-const { argv } = process;
-const timewarriorReportArgument = argv.filter((_, index) => index >= 2);
+#! /usr/bin/env node
 
-console.log(timewarriorReportArgument);
+const { createInterface } = require("readline");
+
+const readline = createInterface({
+  input: process.stdin,
+});
+
+// ini-style configuration, e.g.
+// debug: off
+// reports.day.cell: 15
+// temp.version: 1.4.3
+// verbose: on
+const configurationRegEx = new RegExp(/\w:\s.*$/);
+const gatheredJsonLines = [];
+
+readline.on("line", (line) => {
+  if (line.match(configurationRegEx)) {
+    // Currently nothing to do, we should evaluate timewarrior options like
+    // 'verbose' and 'debug' at some point as recommended by
+    // https://timewarrior.net/docs/api/#guidelines
+  }
+  else if (line === "") {
+    // Configuration block is separated from JSON via a newline which we want to skip
+    // https://timewarrior.net/docs/api/#input-format
+  }
+  else {
+    gatheredJsonLines.push(line);
+  }
+});
+
+readline.once("close", () => {
+  const reportJson = JSON.parse(gatheredJsonLines.join(""));
+  console.log(reportJson);
+});
