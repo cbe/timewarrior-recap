190e1453d8b263068551c23a2ac9b1da47ce8e69
cbe <cbe@users.noreply.github.com>
2023-01-24 14:06:37 +0100

    Enrich returned intervals by duration it took in minutes

--- app/timewarrior-recap.js
+++ app/timewarrior-recap.js
@@ -1,3 +1,13 @@
+const { parseISO, differenceInMinutes } = require("date-fns");
+
 exports.timewarriorRecap = (input) =>
   input
-    .filter((interval) => interval.end);
+    .filter((interval) => interval.end)
+    .map((interval) => {
+      const { id, start: startIso, end: endIso } = interval;
+      const start = parseISO(startIso);
+      const end = parseISO(endIso);
+      const durationInMinutes = Math.abs(differenceInMinutes(start, end));
+
+      return { id, durationInMinutes };
+    });
--- app/timewarrior-recap.spec.js
+++ app/timewarrior-recap.spec.js
@@ -15,7 +15,18 @@ describe("timewarriorRecap", () => {
     const result = timewarriorRecap(input);
     expect(result).toHaveLength(1);
     expect(result[0]).toMatchObject(
-      { id: 3, start: "20230123T081500Z", end: "20230123T103000Z" }
+      { id: 3 }
+    );
+  });
+
+  it("should return the tracked duration in minutes", () => {
+    const input = [
+      { id: 1, start: "20230123T081500Z", end: "20230123T103000Z" }
+    ];
+
+    const result = timewarriorRecap(input);
+    expect(result[0]).toMatchObject(
+      { id: 1, durationInMinutes: 135 }
     );
   });
 });
